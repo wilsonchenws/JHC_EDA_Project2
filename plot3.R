21dabfc6b6e2a92ab837be4cf2d581127f77b37b
wilsonchenws <weishengchen765@gmail.com>
2021-05-08 21:00:57 +0800

    Revise R script for generating plot 3 and re-upload new png file of plot 3.

--- plot3.R
+++ plot3.R
@@ -7,16 +7,12 @@ library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-NEI_By_Year_Type <- group_by(NEI,type,year,.add = F)
+df_for_plot3 <- NEI %>%
+    filter(fips == "24510") %>%
+    group_by(type,year,.add = F) %>%
+    summarise(sum = sum(Emissions)) %>%
+    transform(year = as.factor(year), type = as.factor(type))
 
-df_for_plot3 <- summarise(NEI_By_Year_Type, sum = sum(Emissions))
-df_for_plot3 <- transform(df_for_plot3, year = as.factor(year), type = as.factor(type))
-
-col <- brewer.pal(4,"Blues")
-pal <- colorRampPalette(col,4)
-
-#Using qplot
-qplot(year,sum,data = df_for_plot3,facets = ~type)
 
 #Using ggplot
 
